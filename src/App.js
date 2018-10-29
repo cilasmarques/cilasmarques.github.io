2bcf9a466ed408ae105ba53559ea4954c8cc4d86
cilasmarques <cilas.marques@ccc.ufcg.edu.br>
2018-10-28 22:33:56 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Galery from './components/Galery';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+  constructor(props) {
+    super(props);
+    
+    this.state = "";
+  }
+
+  render()  {
+    return(
+      <div classNamec="App">
       </div>
     );
   }
 }
 
-export default App;
+export default Galery;
