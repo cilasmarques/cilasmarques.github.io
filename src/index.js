a7bf262c6b1ee11e9bfea284eb05ccc0168d487a
cilasmarques <cilas.marques@ccc.ufcg.edu.br>
2019-03-26 20:19:58 -0300

    Num ta abrindo o troco :v

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 import ReactDOM from 'react-dom';
+import React, {Component} from 'react'; 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import AppBar from 'material-ui/AppBar'
-import React, {Component} from 'react';
+import Drawer from 'material-ui/Drawer'
+import {List, ListItem} from 'material-ui/List'
+import './App.css'; 
 
 class App extends Component{
+    constructor () {
+        super();
+        this.state = {
+            drawerOpened: false
+        };
+    };
+
+    _toggleDrawer () {
+        this.setState ({
+            drawerOpened: !this.state.drawerOpened
+        });
+    };
+
     render(){
         return (
-        <MuiThemeProvider> <AppBar/> </MuiThemeProvider>
+            <MuiThemeProvider> 
+                <div>
+                    <AppBar title="Cilânsdia" onClick = {() => _toggleDrawer} /> 
+                    <Drawer open={this.state.drawerOpened} docked={false} onRequestChange={() => this._toggleDrawer}>
+                        <List>
+                            <ListItem> mai rapai </ListItem>
+                            <ListItem> rapai mai</ListItem>
+                        </List>
+                    </Drawer>>
+                </div>
+            </MuiThemeProvider>
+            
         );
     };
 };
