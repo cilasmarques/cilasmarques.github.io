ac026279e01016cd8b43b38ec9a172bab4330c87
cilasmarques <cilas.marques@ccc.ufcg.edu.br>
2019-05-25 22:06:40 -0300

    .

--- src/ExpansionPanel.js
+++ src/ExpansionPanel.js
@@ -17,80 +17,20 @@ const styles = theme => ({
   },
 });
 
-function SimpleExpansionPanel() {
+function SimpleExpansionPanel(props) {
   return (
     <div >
       <ExpansionPanel>
         <ExpansionPanelSummary>
-          <Typography>Python</Typography>
+          <Typography>{props.titulo}</Typography>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
-          <Typography>
-            <Link href="https://www.youtube.com/playlist?list=PL-osiE80TeTt2d9bfVyTiXJA-UTHn6WwU">Corey Schafer: Python Tutorials</Link> 
-            <Link href="https://www.youtube.com/playlist?list=PLi01XoE8jYohWFPpC17Z-wWhPOSuh8Er-">Socratica: Python Programming Tutorials</Link> 
-          </Typography>
+            <Link href={props.link1}><p>{props.nome1}</p></Link> 
+            <Link href={props.link2}><p>{props.nome2}</p></Link> 
+            <text><Link href={props.link3}>{props.nome3}</Link></text> 
+            <text><Link href={props.link4}>{props.nome4}</Link></text> 
         </ExpansionPanelDetails>
       </ExpansionPanel>
-
-      <ExpansionPanel>
-      <ExpansionPanelSummary>
-          <Typography>Java</Typography>
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails>
-          <Typography>
-            <Link href="https://www.youtube.com/playlist?list=PLGxZ4Rq3BOBq0KXHsp5J3PxyFaBIXVs3r">Loiane Groner: Curso de Java Básico Gratuito</Link> 
-          </Typography>
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
-  
-      <ExpansionPanel>
-      <ExpansionPanelSummary>
-          <Typography >HTML and CSS</Typography>
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails>
-          <Typography>
-            <Link href="https://www.youtube.com/playlist?list=PLInBAd9OZCzydDFvm06EgbPXYylGVcyIL">RBTech: HTML e CSS para iniciantes</Link> 
-          </Typography>
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
-  
-      <ExpansionPanel>
-      <ExpansionPanelSummary>
-          <Typography >ReactJS</Typography>
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails>
-          <Typography>
-            <Link href="https://www.youtube.com/playlist?list=PLoYCgNOIyGABj2GQSlDRjgvXtqfDxKm5b">LearnCode.Academy: ReactJS Tutorials</Link> 
-            <Link href="https://www.youtube.com/playlist?list=PLoYCgNOIyGACTDHuZtn0qoBdpzV9c327V">LearnCode.Academy: JavaScript Fundamentals</Link>           
-          </Typography>
-          </ExpansionPanelDetails>
-      </ExpansionPanel>
-  
-      <ExpansionPanel>
-      <ExpansionPanelSummary>
-          <Typography> GIT</Typography>
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails>
-          <Typography>
-            <Link href="https://git-scm.com/book/pt-br/v1/Ramifica%C3%A7%C3%A3o-Branching-no-Git-B%C3%A1sico-de-Branch-e-Merge">Git Branch</Link> 
-            <Link href="http://rogerdudler.github.io/git-guide/index.pt_BR.html">Roger Dudler: Guia Pratico</Link> 
-            <Link href="https://www.youtube.com/playlist?list=PL-osiE80TeTuRUfjRe54Eea17-YfnOOAx">Corey Schafer: Git Tutorials</Link> 
-            <Link href="https://danielkummer.github.io/git-flow-cheatsheet/index.pt_BR.html">Daniel Kummer: Git Flow Cheat-sheet</Link> 
-          </Typography>
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
-  
-      <ExpansionPanel>
-      <ExpansionPanelSummary>
-          <Typography >MarkDown</Typography>
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails>
-          <Typography>
-            <Link href="https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet#links">Adam Pritchard: Markdown cheat-sheet</Link> 
-          </Typography>
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
-  
     </div>
   );}
 
--- src/index.js
+++ src/index.js
@@ -63,8 +63,14 @@ class App extends Component{
                         </Drawer>
                     </AppBar>
                 </div>
+
                 <div>
-                  <SimpleExpansionPanel/>
+                  <SimpleExpansionPanel titulo="Python" link1="https://www.youtube.com/playlist?list=PL-osiE80TeTt2d9bfVyTiXJA-UTHn6WwU" nome1="Corey Schafer: Python Tutorials" link2="https://www.youtube.com/playlist?list=PLi01XoE8jYohWFPpC17Z-wWhPOSuh8Er-" nome2="Socratica: Python Programming Tutorials"/> 
+                  <SimpleExpansionPanel titulo="Java" link1="https://www.youtube.com/playlist?list=PLGxZ4Rq3BOBq0KXHsp5J3PxyFaBIXVs3r" nome1="Loiane Groner: Curso de Java Básico Gratuito"/>
+                  <SimpleExpansionPanel titulo="HTML CSS" link1="https://www.youtube.com/playlist?list=PLInBAd9OZCzydDFvm06EgbPXYylGVcyIL" nome1="RBTech: HTML e CSS para iniciantes"/>
+                  <SimpleExpansionPanel titulo="React JS" link1="https://www.youtube.com/playlist?list=PLoYCgNOIyGABj2GQSlDRjgvXtqfDxKm5b" nome1="LearnCode.Academy: ReactJS Tutorials" link2="https://www.youtube.com/playlist?list=PLoYCgNOIyGACTDHuZtn0qoBdpzV9c327V" nome2="LearnCode.Academy: JavaScript Fundamentals"/>                  
+                  <SimpleExpansionPanel titulo="GIT" link1="https://git-scm.com/book/pt-br/v1/Ramifica%C3%A7%C3%A3o-Branching-no-Git-B%C3%A1sico-de-Branch-e-Merge" nome1="Git Branch" link2="http://rogerdudler.github.io/git-guide/index.pt_BR.html" nome2="Roger Dudler: Guia Pratico" link3="https://www.youtube.com/playlist?list=PL-osiE80TeTuRUfjRe54Eea17-YfnOOAx" nome3="Corey Schafer: Git Tutorials" link4="https://danielkummer.github.io/git-flow-cheatsheet/index.pt_BR.html" nome4="Daniel Kummer: Git Flow Cheat-sheet"/>
+                  <SimpleExpansionPanel titulo="MarkDown" link1="https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet#links" nome1="Adam Pritchard: Markdown cheat-sheet"/>
                 </div>
             </MuiThemeProvider>            
         );
